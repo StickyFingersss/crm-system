dae842d1511d5004fc465de24bb8ca55e1d0194b
StickyFingersss <dynko.valerii@gmail.com>
2023-09-05 20:45:49 +0600

    db seeded

--- server/src/routes/api.router.js
+++ server/src/routes/api.router.js
@@ -1,7 +1,8 @@
 const apiRouter = require('express').Router();
-
+const managerRouter = require('./manager.router');
 const todosRouter = require('./todos.router');
 
 apiRouter.use('/todos', todosRouter);
+apiRouter.use('/managers', managerRouter);
 
 module.exports = apiRouter;
