4c48e2a2af713e291aa8642bc4f58213f9f913f8
Vladislav Salnikov <vladislav.salnikov@icloud.com>
2023-09-13 10:45:03 +0400

    endpoint for all customers fixed

--- server/src/routes/customer.router.js
+++ server/src/routes/customer.router.js
@@ -6,7 +6,6 @@ const { Customer, User, Status } = require('../../db/models');
 
 customerRouter.use('/comment', commentRouter);
 
-
 customerRouter.get('/all', async (req, res) => {
   const { team_id } = req.session;
   try {
@@ -61,95 +60,98 @@ customerRouter.get('/special', async (req, res) => {
         || status_id === 'пустой'
         || status_id === 'empty'
         || status_id === 'Empty'
-        ) {
-          // Используем оператор IS NULL для поиска клиентов со значением status_id, равным null
-          filter.status_id = {
-            [Op.is]: null,
-          };
-        } else {
-          // Ищем запись о статусе с заданным именем
-          const statusData = await Status.findOne({
-            where: { name: status_id },
-          });
-          
-          if (statusData) {
-            // Используем оператор = для поиска клиентов с заданным значением status_id
-            filter.status_id = statusData.id;
-          }
+      ) {
+        // Используем оператор IS NULL для поиска клиентов со значением status_id, равным null
+        filter.status_id = {
+          [Op.is]: null,
+        };
+      } else {
+        // Ищем запись о статусе с заданным именем
+        const statusData = await Status.findOne({
+          where: { name: status_id },
+        });
+
+        if (statusData) {
+          // Используем оператор = для поиска клиентов с заданным значением status_id
+          filter.status_id = statusData.id;
         }
       }
-      // Проверяем наличие ключа manager_id
-      if (manager_id) {
-        // Проверяем значение manager_id
-        if (
-          manager_id === 'Пусто'
+    }
+    // Проверяем наличие ключа manager_id
+    if (manager_id) {
+      // Проверяем значение manager_id
+      if (
+        manager_id === 'Пусто'
           || manager_id === 'пусто'
           || manager_id === 'empty'
           || manager_id === 'Empty'
-          ) {
-            // Показываем всех клиентов с manager_id = null
-            filter.manager_id = null;
-          } else {
-            // Ищем менеджера с заданным именем
-            const manager = await User.findOne({
-              where: { name: manager_id },
-            });
-            
-            if (manager) {
-              // Показываем всех клиентов с текущим manager_id
-              filter.manager_id = manager.id;
-            }
-          }
-        }
-        // Выполняем запрос к базе данных с использованием созданного фильтра
-        const customers = await Customer.findAll({
-          where: {
-            ...filter,
-            team_id,
-          },
-          include: [{ model: Status }],
-          raw: true,
-          nest: true,
+      ) {
+        // Показываем всех клиентов с manager_id = null
+        filter.manager_id = null;
+      } else {
+        // Ищем менеджера с заданным именем
+        const manager = await User.findOne({
+          where: { name: manager_id },
         });
-        res.json(customers);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-    
-    customerRouter.get('/:id', async (req, res) => {
-      try {
-        const { id } = req.params;
-        const customers = await Customer.findOne({
-          where: { id },
-          include: [{ model: Status }],
-          raw: true,
-          nest: true,
-        });
-        res.json(customers);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-    
-    customerRouter.get('/by-manager/:id', async (req, res) => {
-      try {
-        const { userId, isAdmin } = req.session;
-        const { id } = req.params;
-    
-        if (isAdmin || userId) {
-          const customers = await Customer.findAll({
-            where: { manager_id: id },
-          });
-          res.json(customers);
+
+        if (manager) {
+          // Показываем всех клиентов с текущим manager_id
+          filter.manager_id = manager.id;
         }
-      } catch (error) {
-        console.log(error);
       }
+    }
+    // Выполняем запрос к базе данных с использованием созданного фильтра
+    const customers = await Customer.findAll({
+      where: {
+        ...filter,
+        team_id,
+      },
+      include: [{ model: Status }],
+      raw: true,
+      nest: true,
     });
-    
-    customerRouter.put('/manager/:id', async (req, res) => {
-      try {
+    res.json(customers);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+customerRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const customers = await Customer.findOne({
+      where: { id },
+      include: [{ model: Status }],
+      raw: true,
+      nest: true,
+    });
+    res.json(customers);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+customerRouter.get('/by-manager/:id', async (req, res) => {
+  try {
+    const { userId, isAdmin } = req.session;
+    const { id } = req.params;
+    console.log('ID==============>', id);
+    if (isAdmin && id === undefined) {
+      const customers = await Customer.findAll();
+      res.json(customers);
+    } else {
+      const customers = await Customer.findAll({
+        where: { manager_id: id },
+      });
+      res.json(customers);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+customerRouter.put('/manager/:id', async (req, res) => {
+  try {
     const { id } = req.params;
     const { manager_id } = req.body;
     const customer = await Customer.findByPk(id);
